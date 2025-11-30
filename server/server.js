2b1bda24e2d4a7b774a4badf8196706c3208e074
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-30 23:47:14 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ import showRouter from './Routes/showRouter.js';
 import bookingRouter from './Routes/bookingRouter.js';
 import adminRouter from './Routes/adminRouter.js';
 import userRouter from './Routes/userRoutes.js';
+import fileUpload from "express-fileupload";
+import snackRouter from './Routes/SnacksRouter.js';
+import reserveRoute from "./Routes/reserveRoute.js";
+
+
+import dotenv from "dotenv";
+import upcomingRouter from './Routes/upcomingRouter.js';
 
 
 const app = express();
@@ -16,8 +23,11 @@ const port = 3000;
 
 await connectDB()
 
+dotenv.config();
 
 //middleware
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json())
 
@@ -25,8 +35,17 @@ app.use(cors())
 
 app.use(clerkMiddleware());
 
+app.use(fileUpload({
+  useTempFiles: true,
+  tempFileDir: "/tmp/", // optional
+
+}));
+
+
+
 
 //Api Routes
+
 
 app.get('/', (req, res)=> res.send('Server is Live'))
 app.use('/api/inggest',  serve({ client: inngest, functions }))
@@ -34,5 +53,8 @@ app.use('/api/show', showRouter)
 app.use('/api/booking', bookingRouter)
 app.use("/api/admin", adminRouter)
 app.use('/api/user', userRouter)
+app.use('/api/snacks', snackRouter)
+app.use("/api/reserve", reserveRoute)
+app.use('/api/upcoming', upcomingRouter)
 
 app.listen(port, ()=> console.log(  `Server listening at http://localhost:${port}`));
