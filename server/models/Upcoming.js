a63ba1d82080d1333f43c4d29a00863a8008f6fb
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-11-30 23:47:02 +0300

    Create Upcoming.js

---
+++ server/models/Upcoming.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const upcomingSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    backdrop_path: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+    description : {
+        type: String,
+        required: true
+    },
+    release_date: { type: String, required: true },
+    language: { type: String },
+    genres: { type: [String], required: true },
+    casts: [
+      {
+        name: { type: String, required: true },
+        castsImage: { // 👈 changed here
+          public_id: { type: String, required: true },
+          url: { type: String, required: true },
+        },
+      },
+    ],
+    runtime: { type: Number, required: true },
+    trailer: {
+      public_id: { type: String, required: true },
+      url: { type: String, required: true },
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Upcoming", upcomingSchema);
