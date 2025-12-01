08cb7f63255d672714902b2a163ec4a8ef5d21dc
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-12-01 23:21:08 +0300

    Update reserveRoute.js

--- server/Routes/reserveRoute.js
+++ server/Routes/reserveRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createReservation, getAllReservations } from "../controllers/reserveController.js";
+import { createReservation, getAllReservations, approveReservation, deleteReservation } from "../controllers/reserveController.js";
+import { protectAdmin } from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/Add", createReservation);
 router.get('/get', getAllReservations);
+router.patch("/approve/:id", approveReservation);
+router.delete("/delete/:id", deleteReservation);
 
 export default router;
