74e3a28dbbe2b272c739bcd5604ef23ef7e50ee5
Ezana Mulugeta <Ezanamulugeta89@gmail.com>
2025-12-09 20:16:39 +0300

    Update Movies.jsx

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -12,9 +12,12 @@ const Movies = () => {
         <div className='relative my-40 mb-20 px-6 md:px-16 lg:px-40 xl:px-44 overflow-hidden min-h-[80vh]'>
             <h1 className='text-lg font-medium my-4'>Now Showing</h1>
             <div className='flex flex-wrap max-sm:justify-center gap-8'>
-                {shows.map((movie) => (
-                    <MovieCard  movie={movie} key={movie._id}/>
-                ))}
+                {shows
+  ?.filter((movie) => movie && movie._id)
+  .map((movie) => (
+    <MovieCard movie={movie} key={movie._id} />
+  ))}
+
             </div>
             
         </div>
